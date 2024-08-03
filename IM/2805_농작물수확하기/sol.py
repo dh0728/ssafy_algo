9a05f19a4456cd1d1e1687aaf632c9d646980701
dh0728 <dustkscjswo@naver.com>
2024-08-03 16:46:45 +0900

    0803_2

---
+++ IM/2805_농작물수확하기/sol.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin=open('input.txt')
+
+def income(N,arr,dy):
+    # 중앙열부터 세기 시작
+    # 한줄씩 위 혹은 밑으로 열이 이동할 때마다 세는 칸의 수가 두칸씩 감소
+    for i in range
+
+T=int(input())
+for tc in range(1, T+1):
+    N=int(input())
+    arr=[list(map(int,input().split())) for _ in range(N)]
+    dy=[1,-1]
+    result=income(N,arr,dy)
+
