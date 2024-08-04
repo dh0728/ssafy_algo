ae73197eab36eba743b4325d4cd82e0fb14b8319
dh0728 <dustkscjswo@naver.com>
2024-08-05 00:08:56 +0900

    0805

--- IM/19590_마술사의사냥/sol.py
+++ IM/19590_마술사의사냥/sol.py
@@ -1,6 +1,6 @@
 import sys
 sys.stdin = open('input.txt')
-#dfs 어렵넹
+
 def magic(arr, K, N):
     dxy=[[1,1],[-1,1],[-1,-1],[1,-1]] #오른쪽위, 오른쪽아래, 왼쪽아래, 왼쪽위(시계방향)
     max_cnt=0   #최고값
@@ -25,3 +25,26 @@ arr = [list(map(int, input().split())) for _ in range(N)]
 K=int(input())
 result= magic(arr,K,N)
 print(result)
+
+
+# N = int(input())
+# arr = [list(map(int, input().split())) for _ in range(N)]
+# K = int(input())
+#
+# di = [-1, 1, 1, -1]
+# dj = [1, 1, -1, -1]
+#
+# max_value = 0
+# for i in range(N):
+#     for j in range(N):
+#         S = 0
+#         for e in range(len(di)):
+#             for k in range(1,K+1):
+#                 ni = i + di[e]*k #di[e]=-1 k=1 ni= i + -1, k=2 ni= i + -1 + -1
+#                 nj = j + dj[e]*k #di[e]=1
+#                 if 0 <= ni < N and 0 <= nj < N:
+#                     S += arr[ni][nj]
+#         if max_value < S:
+#             max_value = S
+#
+# print(f'{max_value}')
