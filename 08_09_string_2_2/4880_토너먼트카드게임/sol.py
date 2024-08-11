112af12343ec96d86ea14a32d01b08a74be29951
dh0728 <dustkscjswo@naver.com>
2024-08-11 23:41:38 +0900

    08_11_2

--- 08_09_string_2_2/4880_토너먼트카드게임/sol.py
+++ 08_09_string_2_2/4880_토너먼트카드게임/sol.py
@@ -60,57 +60,44 @@ sys.stdin = open('input.txt')
 #     print(f'#{tc} {win}')
 #     # 가위 1, 바위 2, 보 3
 
-def ro_pa_scis(arr,st):
-    global win
-    l=len(arr)
-    arr2 = []  # 이긴 사람의 카드가 들어가는 리스트
-    st2 = []  # 이긴 사람의 번호가 들어가는 인덱스
-    if l>=3:    # 사람 수가 3명 이상
-        for i in range(0,l//2,2):   #앞그룹
-            if i+1 == l//2:   # 가위바위보 할 사람이 없을 경우
-                arr2.append(arr[i])
-                st2.append(st[i])
-            elif arr[i]-arr[i+1]==0:  #비기는 경우 번호가 낮은 사람이 들어감
-                arr2.append(arr[i])
-                st2.append(st[i])
-            elif (arr[i] - arr[i + 1]) % 3 == 1:  # 앞사람이 이기는 경우
-                arr2.append(arr[i])
-                st2.append(st[i])
-            else:                                 # 뒷사람이 이기는 경우
-                arr2.append(arr[i+1])
-                st2.append(st[i+1])
-        for i in range(l//2,l,2):   # 두번째 그룹
-            print('그룹2', i)
-            if i+1 == l:   # 가위바위보 할 사람이 없을 경우
-                arr2.append(arr[i])
-                st2.append(st[i])
-            elif arr[i]-arr[i+1]==0:  #비기는 경우 번호가 낮은 사람이 들어감
-                arr2.append(arr[i])
-                st2.append(st[i])
-            elif (arr[i] - arr[i + 1]) % 3 == 1:  # 앞사람이 이기는 경우
-                arr2.append(arr[i])
-                st2.append(st[i])
-            else:                               # 뒷사람이 이기는 경우
-                arr2.append(arr[i+1])
-                st2.append(st[i+1])
-        ro_pa_scis(arr2,st2)
-    if l==2:
-        if arr[0]-arr[1] == 0:  # 비기는 경우 번호가 낮은 사람이 들어감
-            win=st[0]+1         # 비기는 경우 먼저 뒤에 가면 틀림
-            return
-        elif (arr[0]-arr[1])%3 == 1:
-            win=st[0]+1
-            return
-        win=st[1]+1
-        return
+def ro_pa_scis2(arr,st):    # 가위바위보 하는 함수
+    l = len(arr)    # 가위바위보 할 사람 수
+    arr_win=[]  # 승자 카드가 들어가는 리스트
+    st_win=[]   # 승자 번호가 들어가는 리스트
+    if l==1:
+        return [arr[0],st[0]]
+    for i in range(0,l,2):
+        if i+1 == l:   # 가위바위보 할 사람이 없을 경우
+            arr_win.append(arr[i])
+            st_win.append(st[i])
+        elif arr[i]-arr[i+1]==0:  #비기는 경우 번호가 낮은 사람이 들어감
+            arr_win.append(arr[i])
+            st_win.append(st[i])
+        elif (arr[i] - arr[i + 1]) % 3 == 1:  # 앞사람이 이기는 경우
+            arr_win.append(arr[i])
+            st_win.append(st[i])
+        else:                                 # 뒷사람이 이기는 경우
+            arr_win.append(arr[i+1])
+            st_win.append(st[i+1])
+    return ro_pa_scis2(arr_win,st_win)        # 승자끼리 다시 가위바위보
+
+def group(arr,st):  # 그룹 나누는 함수
+    win1=ro_pa_scis2(arr[:N//2], st[:N//2])  # 그룹 나누기
+    win2=ro_pa_scis2(arr[N//2:], st[N//2:])
+    result = win1[0]-win2[0]                 # 나눈 그룹 승자들 뽑아서 다시 가위바위보
+    if result == 0:  # 비기는 경우 번호가 낮은 사람이 들어감
+        return win1[1]+1
+    elif result % 3 == 1:  # 앞사람이 이기는 경우
+        return win1[1]+1
+    return win2[1]+1
 
 T= int(input())
 for tc in range(1,T+1):
-    win= 0
     N = int(input())
     arr= list(map(int, input().split()))
     st= [n for n in range(N)]
-    ro_pa_scis(arr[:N//2], st)
-    print(f'#{tc} {win}')
+    result=group(arr,st)
+    print(f'#{tc} {result}')
     # 가위 1, 바위 2, 보 3
+
 
--- 1tassafy/findangle.py
+++ 1tassafy/findangle.py
@@ -1,22 +1,30 @@
 import math
 def find_angle(myball,target):
-    x=hole4[0]-myball[0]
-    y=hole4[1]-myball[1]
-    ga=math.atan(y/x)   #가
+    x=abs(hole4[0]-myball[0])
+    y=abs(hole4[1]-myball[1])
+    ga=math.atan2(y,x)   #가
 
     # 공가 홀 사이의 거리 구하기(a, b, c)
     my_dis=math.sqrt((hole4[0]-myball[0])**2+(hole4[1]-myball[1])**2)  # 내공과 홀 사이의 거리 a
     tar_dis = math.sqrt((hole4[0] - target[0]) ** 2 + (hole4[1] - target[1]) ** 2)  # 목적구 와 홀 사이의 거리 b
     my_tar_dis = math.sqrt((myball[0] - target[0]) ** 2 + (myball[1] - target[1]) ** 2)  # 내공과 목적구 사이의 거리 c
+    print(f'내공과 홀 사이 거리 :{my_dis}')
+    print(f'목적구와 홀 사이 거리 :{tar_dis}')
+    print(f'내공과 목적구 사이거리 :{my_tar_dis}')
 
     # 다 구하기
     da=math.acos((my_dis**2+tar_dis**2-my_tar_dis**2)/(2*my_dis*tar_dis)) #다
-
     # d 구하기 제 2 코사인 법칙
+    print(f'다 : {math.degrees(da)}')
     d= math.sqrt((my_dis**2+(tar_dis+r2)**2)-2*my_dis*(tar_dis+r2))
-
     # 나 구하기 제2 코사인 법칙 이용
-    na=math.acos((my_dis**2+d**2-(tar_dis+r2)**2)/(2*my_dis*d))
+    print(f'd : {d}')
+    na_angle=(my_dis**2+d**2-(tar_dis+r2)**2)/(2*my_dis*d)
+    if na_angle >1:
+        na_angle=1
+    elif na_angle <-1:
+        na_angle=-1
+    na=math.acos(na_angle)
 
     angle=ga+na
     result=math.degrees(angle)
