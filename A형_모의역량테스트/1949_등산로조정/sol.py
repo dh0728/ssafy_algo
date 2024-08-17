9d2783593eaad668a79a2d57d33d244467581b32
dh0728 <dustkscjswo@naver.com>
2024-08-17 23:43:17 +0900

    0817

--- A형_모의역량테스트/1949_등산로조정/sol.py
+++ A형_모의역량테스트/1949_등산로조정/sol.py
@@ -1,15 +1,32 @@
 import sys
 sys.stdin=open('input.txt')
 
+def bfs(x,y):
+    print(x,y)
+    pass
+
 def find_top():
     max_top=0
     for i in range(N):
         for j in range(N):
             if maps[i][j]>max_top:
                 max_top=maps[i][j]
+    return max_top
+
+def start(top):
+    cnt=0
+    for i in range(N):
+        for j in range(N):
+            if cnt==5:
+                return
+            if maps[i][j]==top:
+                bfs(i,j)
+                cnt+=1
+    return
 
 T=int(input())
 for tc in range(1,T+1):
     N,K=map(int,input().split())
     maps=[list(map(int,input().split())) for _ in range(N)]
-    find_top()
+    max_top=find_top()
+    start(max_top)
