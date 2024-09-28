41d48d9920521cd6686a80770e0fd753f039d067
dh0728 <dustkscjswo@naver.com>
2024-09-28 23:48:37 +0900

    ex

--- ex/sol.py
+++ ex/sol.py
@@ -1,10 +1,23 @@
 import sys
 sys.stdin=open('input.txt')
+# input = sys.stdin.readline
 
-while True:
-    try:
-        print(input())
-    except:
-        break
+def exam_square(n):
+    color=arr[n][n]
+    for i in range(n):
+        for j in range(n):
+            if arr[i][j] != color: # 다른 색의 종이가 있다면
+                return 0
+    return 1
+
+
+# N = 2 , 4, 8, 16, 32, 64, 128
+N= int(input())
+arr=[list(map(int,input().split())) for _ in range(N)]
+
+blue_cnt=0
+white_cnt=0
+cut=N
+while cut != 0:
 
 
