2314827fa73acf3ccbee0734927ae27cf20a1fd8
dh0728 <dustkscjswo@naver.com>
2024-10-18 21:07:27 +0900

    d

--- ex/sol.py
+++ ex/sol.py
@@ -1,23 +1,22 @@
 import sys
 sys.stdin=open('input.txt')
-# input = sys.stdin.readline
-
-def exam_square(n):
-    color=arr[n][n]
-    for i in range(n):
-        for j in range(n):
-            if arr[i][j] != color: # 다른 색의 종이가 있다면
-                return 0
-    return 1
+#input = sys.stdin.readline
 
 
-# N = 2 , 4, 8, 16, 32, 64, 128
-N= int(input())
+N = int(input())
 arr=[list(map(int,input().split())) for _ in range(N)]
+result=0
 
-blue_cnt=0
-white_cnt=0
-cut=N
-while cut != 0:
+for i in range(1,N):
+    for j in range(i+1):
+        if j==0: # 왼쪽 끝이면
+            arr[i][j] +=arr[i-1][j]
+        elif j==i: # 오른쪽 끝이면
+            arr[i][j] +=arr[i-1][j-1]
+        else:
+            arr[i][j] += max(arr[i-1][j-1], arr[i-1][j]) #윗줄에 왼쪽 오른쪽중 더큰 것 넣기
+
+print(max(arr[N-1]))
+
 
 
